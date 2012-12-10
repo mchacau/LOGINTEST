8379e74ef966ce3ce4cfa63e7c9454c14da74fdd
Matt Needham <matt@mchac.com.au>
2012-12-10 22:57:38 +1000

    update

--- js/main.js
+++ js/main.js
@@ -50,6 +50,38 @@ function showAlert() {
   localStorage.removeItem("username");
   localStorage.removeItem("password");
   $.mobile.changePage("index.html");
-          $("#username", form).val();
-        $("#password", form).val();
     }
+    
+    
+    function ajaxFunction(){
+    var ajaxRequest;  // The variable that makes Ajax possible!
+
+    try{
+        // Opera 8.0+, Firefox, Safari
+        ajaxRequest = new XMLHttpRequest();
+    } catch (e){
+        // Internet Explorer Browsers
+        try{
+            ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try{
+                ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e){
+                // Something went wrong
+                alert("Your browser broke!");
+                return false;
+            }
+        }
+    }
+    // Create a function that will receive data sent from the server
+    ajaxRequest.onreadystatechange = function(){
+        if(ajaxRequest.readyState == 4){
+            var ajaxDisplay = document.getElementById('ajaxDiv');
+            ajaxDisplay.innerHTML = ajaxRequest.responseText;
+        }
+    }
+    var age = window.localStorage["username"];
+    var queryString = "?age=" + age ;
+    ajaxRequest.open("GET", "http://www.mchac.com.au/aktiv/index.php" + queryString, true);
+    ajaxRequest.send(null); 
+}
